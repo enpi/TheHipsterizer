56387f201c133097b426fc56955d98894e69fca4
julioxus <julioxus@gmail.com>
2015-02-17 20:10:58 +0100

    Rediseñado proyecto en Android Studio

---
+++ app/src/main/java/com/codamasters/thehipsterizer/MainActivity.java
@@ -0,0 +1,207 @@
+package com.codamasters.thehipsterizer;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.PictureCallback;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private PictureCallback mPicture;
+    private ImageButton capture, switchCamera;
+    private Context myContext;
+    private LinearLayout cameraPreview;
+    private boolean cameraFront = false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        myContext = this;
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        initialize();
+    }
+
+    private int findFrontFacingCamera() {
+        int cameraId = -1;
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            CameraInfo info = new CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
+                cameraId = i;
+                cameraFront = true;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+    private int findBackFacingCamera() {
+        int cameraId = -1;
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            CameraInfo info = new CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == CameraInfo.CAMERA_FACING_BACK) {
+                cameraId = i;
+                cameraFront = false;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+    public void onResume() {
+        super.onResume();
+        if (!hasCamera(myContext)) {
+            Toast toast = Toast.makeText(myContext, "Sorry, your phone does not have a camera!", Toast.LENGTH_LONG);
+            toast.show();
+            finish();
+        }
+        if (mCamera == null) {
+            if (findFrontFacingCamera() < 0) {
+                Toast.makeText(this, "No front facing camera found.", Toast.LENGTH_LONG).show();
+                switchCamera.setVisibility(View.GONE);
+            }
+            mCamera = Camera.open(findBackFacingCamera());
+            mPicture = getPictureCallback();
+            mPreview.refreshCamera(mCamera);
+        }
+    }
+
+    public void initialize() {
+        cameraPreview = (LinearLayout) findViewById(R.id.camera_preview);
+        mPreview = new CameraPreview(myContext, mCamera);
+        cameraPreview.addView(mPreview);
+
+        capture = (ImageButton) findViewById(R.id.button_capture);
+        capture.setOnClickListener(captureListener);
+
+        switchCamera = (ImageButton) findViewById(R.id.button_ChangeCamera);
+        switchCamera.setOnClickListener(switchCameraListener);
+    }
+
+    OnClickListener switchCameraListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int camerasNumber = Camera.getNumberOfCameras();
+            if (camerasNumber > 1) {
+
+                releaseCamera();
+                chooseCamera();
+            } else {
+                Toast toast = Toast.makeText(myContext, "Sorry, your phone has only one camera!", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+    };
+
+    public void chooseCamera() {
+        if (cameraFront) {
+            int cameraId = findBackFacingCamera();
+            if (cameraId >= 0) {
+                mCamera = Camera.open(cameraId);
+                mPicture = getPictureCallback();
+                mPreview.refreshCamera(mCamera);
+            }
+        } else {
+            int cameraId = findFrontFacingCamera();
+            if (cameraId >= 0) {
+
+                mCamera = Camera.open(cameraId);
+                mPicture = getPictureCallback();
+                mPreview.refreshCamera(mCamera);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
+    }
+
+    private boolean hasCamera(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private PictureCallback getPictureCallback() {
+        PictureCallback picture = new PictureCallback() {
+
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                File pictureFile = getOutputMediaFile();
+
+                if (pictureFile == null) {
+                    return;
+                }
+                try {
+                    FileOutputStream fos = new FileOutputStream(pictureFile);
+                    fos.write(data);
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                } catch (IOException e) {
+                }
+                mPreview.refreshCamera(mCamera);
+            }
+        };
+        return picture;
+    }
+
+    OnClickListener captureListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mCamera.takePicture(null, null, mPicture);
+        }
+    };
+
+    private static File getOutputMediaFile() {
+        File mediaStorageDir = new File("/sdcard/", "The Hipsterizer");
+
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+        return mediaFile;
+    }
+
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+}
