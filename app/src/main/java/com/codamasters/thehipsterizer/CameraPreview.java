c807d4eaef57378cfeb300b28f84cf5c7baf9feb
julioxus <julioxus@gmail.com>
2015-02-19 15:59:54 +0100

    Arreglo parcial de orientación de la cámara #1

--- app/src/main/java/com/codamasters/thehipsterizer/CameraPreview.java
+++ app/src/main/java/com/codamasters/thehipsterizer/CameraPreview.java
@@ -6,19 +6,26 @@ import android.content.Context;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.WindowManager;
 
 public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
 	private SurfaceHolder mHolder;
 	private Camera mCamera;
     private String currentFilter = Parameters.EFFECT_NONE;
+    private Context context;
+    boolean isPreviewRunning;
 
 	public CameraPreview(Context context, Camera camera) {
 		super(context);
 		mCamera = camera;
 		mHolder = getHolder();
 		mHolder.addCallback(this);
+        isPreviewRunning = false;
+        this.context = context;
 		// deprecated setting, but required on Android versions prior to 3.0
 		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 	}
@@ -29,6 +36,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 			if (mCamera == null) {
 				mCamera.setPreviewDisplay(holder);
 				mCamera.startPreview();
+                isPreviewRunning = true;
 			}
 		} catch (IOException e) {
 			Log.d(VIEW_LOG_TAG, "Error setting camera preview: " + e.getMessage());
@@ -43,12 +51,14 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 		// stop preview before making changes
 		try {
 			mCamera.stopPreview();
+            isPreviewRunning = false;
 		} catch (Exception e) {
 			// ignore: tried to stop a non-existent preview
 		}
 		// set preview size and make any resize, rotate or
 		// reformatting changes here
 		// start preview with new settings
+
 		setCamera(camera);
 		try {
 			mCamera.setPreviewDisplay(mHolder);
@@ -56,6 +66,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 			params.setColorEffect(currentFilter );
 			mCamera.setParameters(params);
 			mCamera.startPreview();
+            isPreviewRunning = true;
 		} catch (Exception e) {
 			Log.d(VIEW_LOG_TAG, "Error starting camera preview: " + e.getMessage());
 		}
@@ -69,15 +80,48 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         return currentFilter ;
     }
 
-	public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-		// If your preview can change or rotate, take care of those events here.
-		// Make sure to stop the preview before resizing or reformatting it.
-		refreshCamera(mCamera);
-	}
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        if (isPreviewRunning)
+        {
+            mCamera.stopPreview();
+            isPreviewRunning = false;
+        }
+
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+
+        if(display.getRotation() == Surface.ROTATION_0)
+        {
+            mCamera.setDisplayOrientation(90);
+        }
+
+        if(display.getRotation() == Surface.ROTATION_90)
+        {
+
+        }
+
+        if(display.getRotation() == Surface.ROTATION_180)
+        {
+
+        }
+
+        if(display.getRotation() == Surface.ROTATION_270)
+        {
+
+            mCamera.setDisplayOrientation(180);
+        }
+
+        refreshCamera(mCamera);
+
+        mCamera.startPreview();
+        isPreviewRunning = true;
+
+    }
 
 	public void setCamera(Camera camera) {
 		//method to set a camera instance
 		mCamera = camera;
+
 	}
 
 	@Override
