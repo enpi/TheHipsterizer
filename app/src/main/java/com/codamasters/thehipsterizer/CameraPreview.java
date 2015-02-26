e4fb1c5627170f7c80c7afd779da9915e8542993
enpi <juammanc@hotmail.com>
2015-02-26 19:01:47 +0100

    Enpi

--- app/src/main/java/com/codamasters/thehipsterizer/CameraPreview.java
+++ app/src/main/java/com/codamasters/thehipsterizer/CameraPreview.java
@@ -1,8 +1,16 @@
 package com.codamasters.thehipsterizer;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ImageFormat;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.util.Log;
@@ -12,7 +20,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.WindowManager;
 
-public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback, Camera.PreviewCallback {
 	private SurfaceHolder mHolder;
 	private Camera mCamera;
     private String currentFilter = Parameters.EFFECT_NONE;
@@ -29,7 +37,8 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         this.context = context;
 		// deprecated setting, but required on Android versions prior to 3.0
 		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-	}
+        setWillNotDraw(false);
+    }
 
 	public void surfaceCreated(SurfaceHolder holder) {
 		try {
@@ -138,4 +147,35 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 		// mCamera.release();
 
 	}
+
+    private byte[] cameraFrame;
+    private byte[] buffer;
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        cameraFrame = data;
+        camera.addCallbackBuffer(data); //actually, addCallbackBuffer(buffer) has to be called once sowhere before you call mCamera.startPreview();
+    }
+
+
+    private ByteArrayOutputStream baos;
+    private YuvImage yuvimage;
+    private byte[] jdata;
+    private Bitmap bmp;
+    private Paint paint;
+
+    @Override //from SurfaceView
+    public void onDraw(Canvas canvas) {
+        /*
+        baos = new ByteArrayOutputStream();
+        yuvimage=new YuvImage(cameraFrame, ImageFormat.NV21, prevX, prevY, null);
+
+        yuvimage.compressToJpeg(new Rect(0, 0, width, height), 80, baos); //width and height of the screen
+        jdata = baos.toByteArray();
+
+        bmp = BitmapFactory.decodeByteArray(jdata, 0, jdata.length);
+
+        canvas.drawBitmap(bmp , 0, 0, paint);
+        invalidate(); //to call ondraw again
+        */
+    }
 }
